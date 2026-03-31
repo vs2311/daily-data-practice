46d59c8abf41c053cc9ecad16fc3dd3a637f82b8
Vijay Saroj <23je1085@gmail.com>
2026-04-01 00:07:00 +0530

    Initial SQL practice

--- sql/Project employee I .sql
+++
@@ -1,9 +0,0 @@
-select  p.project_id ,round(avg(e.experience_years),2) as average_years 
-from Project p
-join Employee e
-on p.employee_id=e.employee_id
-group by p.project_id
-
- -- Learnings Here
- --  round(av_value(),2) >> upto 2 decimal
- 
